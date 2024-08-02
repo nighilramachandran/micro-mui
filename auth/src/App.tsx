d5f222c478ae6a81f22cf5528fa1563afc52aaa6
Nighil <nighilramm@gmail.com>
2024-08-02 21:11:13 +0400

    fix auth issue in browser

--- auth/src/App.tsx
+++ auth/src/App.tsx
@@ -2,13 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
-import Login from "./exports/auth/Login";
+import { StoreProvider } from "store/Store";
+import GoogleAuthProvider from "./exports/provider/GoogleAuthProvider";
 
-const App = () => <Login />;
+const App = () => (
+  <div className="container">
+    <div>Name: Auth</div>
+    <div>Framework: react</div>
+    <div>Language: TypeScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+);
 
 const rootElement = document.getElementById("app");
 if (!rootElement) throw new Error("Failed to find the root element");
 
 const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
-root.render(<App />);
+root.render(
+  <GoogleAuthProvider>
+    <StoreProvider>
+      <App />
+    </StoreProvider>
+  </GoogleAuthProvider>
+);
